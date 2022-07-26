a7fe2aaabfdcc976a5e465f305313c354651a4c3
Brandt Bessell <60202219+babessell1@users.noreply.github.com>
2022-07-26 19:12:53 -0400

    Update linear_systems.py

--- src/cobamp/core/linear_systems.py
+++ src/cobamp/core/linear_systems.py
@@ -245,6 +245,7 @@ class LinearSystem():
 			self.model.problem.Params.Threads = actual_threads
             self.model.problem.Params.Seed = 12345
             self.model.problem.Params.TimeLimit = gurobipy.GRB.INFINITY
+            print("RANDOM SEEEEEEEEEEEEEEEEEEEEEEEEEEEEEED")
 		else:
 			warnings.warn('Could not set threads for ' + str(self.solver) + ' instance. Not yet implemented!')
 
